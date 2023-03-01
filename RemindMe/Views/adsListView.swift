56af3b68e0c426939eb7c2523006636b1108b389
digiDreams <digital2dreamer@gmail.com>
2023-03-01 12:29:05 +0100

    Creating TaskApp project from scratch with CoreData

---
+++ RemindMe/Views/adsListView.swift
@@ -0,0 +1,20 @@
+//
+//  adsListView.swift
+//  RemindMe
+//
+//  Created by Apprenant 16 on 01/03/2023.
+//
+
+import SwiftUI
+
+struct adsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct adsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        adsListView()
+    }
+}
---
+++ RemindMe/Views/adsWheelView.swift
@@ -0,0 +1,20 @@
+//
+//  adsWheelView.swift
+//  RemindMe
+//
+//  Created by Apprenant 16 on 01/03/2023.
+//
+
+import SwiftUI
+
+struct adsWheelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct adsWheelView_Previews: PreviewProvider {
+    static var previews: some View {
+        adsWheelView()
+    }
+}
