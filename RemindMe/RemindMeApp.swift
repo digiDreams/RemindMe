563b4fe39cd5163119ebb5520b743b5b9283cdac
digiDreams <digital2dreamer@gmail.com>
2023-03-01 12:14:04 +0100

    Initial Commit

---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Apprenant 16 on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
